2737a4ef5a1cf59c4ce0d9eaaf38381cd9e70e0c
dhackelman <drew.hackelman@gmail.com>
2017-01-25 18:04:33 -0500

    Dice roller is working. Images update. Some info logs on screen, other still in console

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # the-iron-yard-HW-dice-roller
 Roll the dice and 7/11 win. JavaScript HW
+
+Dice by Dice by Christian Mohr from the Noun Project (https://thenounproject.com/search/?q=dice&i=110902)
---
+++ scripts/main.js
@@ -0,0 +1,77 @@
+const dropDice = {
+  rollerBtn: document.querySelector('.roller'),
+  dice1: document.querySelector('.dice-one'),
+  dice1Image: null,
+  dice2: document.querySelector('.dice-two'),
+  dice2Image: null,
+  dice1Array: [
+    "images/dice-1.svg",
+    "images/dice-2.svg",
+    "images/dice-3.svg",
+    "images/dice-4.svg",
+    "images/dice-5.svg",
+    "images/dice-6.svg"
+  ],
+  dice2Array: [
+    "images/dice-1-copy.svg",
+    "images/dice-2-copy.svg",
+    "images/dice-3-copy.svg",
+    "images/dice-4-copy.svg",
+    "images/dice-5-copy.svg",
+    "images/dice-6-copy.svg"
+  ],
+  i: 0,
+  userScore: 0,
+  gameOutcome: document.querySelector('.game-stats'),
+
+  rounds: {
+      //store information about round 1 (outcome: win or loss)
+      //store "round" + i + "outcome=" win or lose?
+  },
+
+  //run randomArray generator for dice 1 and assign it to array index
+  rollDice() {
+    //I can't just use the rand# for my array, I need to store those values each time to make comparisons/calculations
+    let rand1 = Math.floor(Math.random()*6)+0;
+    let rand2 = Math.floor(Math.random()*6)+0;
+    this.userScore= rand1 + rand2;
+    this.dice1Image = this.dice1Array[rand1];
+    this.dice2Image = this.dice2Array[rand2];
+    this.i++;
+  },
+
+  winOrLose() {
+    //I need to calculate totals of images (i.e. I need to store their value, not just use it)
+    //don't forget to add rand #s +2 because rand #s were zero-indexed to fit array
+    //how do I get winOrLose to reset on every click event?
+    console.log(this.userScore + 2);
+
+    if (this.userScore+2 === 7) {
+      this.gameOutcome.innerHTML = 'winner';
+    } else if (this.userScore+2 === 11) {
+      this.gameOutcome.innerHTML = 'Winner';
+    } else {
+      this.gameOutcome.innerHTML = 'Roll Again!';
+    }
+  },
+
+  updateDiceImages() {
+    this.dice1.innerHTML = "<img src=" + this.dice1Image + ">";
+    this.dice2.innerHTML = "<img src=" + this.dice2Image + ">";
+  },
+
+
+
+  init() {
+    this.rollerBtn.addEventListener('click', () => {
+      event.preventDefault();
+
+      this.rollDice();
+      this.updateDiceImages();
+      this.winOrLose();
+
+    })
+  }
+}
+
+dropDice.init();
